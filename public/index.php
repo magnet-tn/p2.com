aed071020e598da3df7bba7a4bbd2d9f3ec7cd63
Thomas Narro <Thomas.Narro@gmail.com>
2016-09-28 05:23:17 -0400

    application with all form elements and hardcode word array and special chars.

--- public/index.php
+++ public/index.php
@@ -1,38 +1,56 @@
 <?php
 error_reporting(E_ALL);       // Report Errors, Warnings, and Notices
-    ini_set('display_errors', 1); // Display errors on page (instead of a log file)
+ini_set('display_errors', 1); // Display errors on page (instead of a log file)
 ?>
-
-    //
-<?php
-    // phpinfo();
- ?>
-
 
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="UTF-8">
-    <title>memory game</title>
-    <style>
-        /* We'll use this class to style the boxes..Gold, 50x50px */
 
-        .box {
-            width: 100px;
-            height: 100px;
-            float: left;
-            margin: 4px;
-            padding: 5px;
-            background-color: #ffbf00;
-        }
-    </style>
+    <link rel='stylesheet' href='styles.css' type='text/css'>
+    <link rel="icon" type="image/png" href="TroubleU-icon.png">
+    
+    <title>=Password Generator</title>
+
+    <?php require 'logic.php'; ?>
 </head>
 
 <body>
-    <?php require 'logic.php'; ?>
-    <h1>This is project 2 - p2</h1>
-    <?php echo $boxes; ?>
+    <h1>Password Generator - P2</h1>
+
+    <form action='index.php' method='GET'>
+
+        <input type='integer' name='wordQty'><br>
+        <input type='text' name='characterQty'><br>
+        <label>Use Special Character</label>
+        <input type="radio" name="inclChars" value="yes">yes
+        <input type="radio" name="inclChars" value="no" checked>no<br>
+        <label>Use Number</label>
+        <input type="radio" name="inclNum" value="yes">yes
+        <input type="radio" name="inclNum" value="no" checked>no<br><br>
+
+        <input type='submit' value='Generate Special Password'>
+        <br><br>
+
+    </form>
+
+    <table>
+        <tr>
+            <th width="50">Index</th>
+            <th width="100">Word</th>
+        </tr>
+        <?php foreach($random_words as $idx => $pword): ?>
+            <tr>
+                <td><?php echo $idx ?></td>
+                <td><?php echo $wordArray[$pword] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+
+    <br>
+
 </body>
 
 </html>
--- public/logic.php
+++ public/logic.php
@@ -1,6 +1,47 @@
-/* logic.php */
 <?php
-$boxes = "";
-for($i = 0; $i < 1000; $i++) {
-    $boxes = $boxes."<div class='box'>".$i."</div>";
+# logic.php
+
+// echo '<pre>';
+// var_dump($_GET);
+// echo '</pre>';
+
+//$contestants = [];
+
+//special characters array
+$charArray = ['~','!','@','#', '$', '%', '^', '&', '*', '(', ')', '-'];
+array_push($charArray,'_', '=', '+', '[', '{', ']', '}', '|', ':', ':', '"');
+array_push($charArray, "'", ',', '<', '.', '>', '/', '?');
+
+// test word array
+$wordArray = ['pumpkin', 'card', 'age', 'formal', 'angle', 'ghost', 'fire', 'archive', 'bladder', 'mangle', 'north', 'fusion', 'garter', 'whistle', 'cave', 'governor', 'still', 'riddle', 'tan', 'grift', 'aloe', 'plant', 'frog','mile', 'sift', 'bring', 'trap', 'mallice'];
+// echo "<pre>";
+var_dump($_GET, $charArray);
+// echo "</pre>";
+echo '<br><br>';
+
+if ($_GET['inclNum'] == 'yes'){
+    $pNum = rand(0,9);
+    echo $pNum;
+} else {
+    $pNum = "" ;
+    echo "no num".$pNum;
 }
+
+$numWords = $_GET["wordQty"];
+$random_words=array_rand($wordArray,$numWords);
+
+
+// for ($x = 0; $x <= 5; $x++) {
+//     echo "The number is: $x <br>";
+// }
+
+// foreach($_GET as $inputContestant => $contestantName) {
+//     if($contestantName !=""){
+//         $contestants[$contestantName] = 'loser';
+//     }
+// }
+// $winner = array_rand($contestants,1);
+// $contestants[$winner] = "winner";
+// if($winner == ""){
+//     $contestants[$winner] = "none";
+// }
