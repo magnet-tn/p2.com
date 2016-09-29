7f56da1a907d185c23ce31ca518121d252c56aeb
Thomas Narro <Thomas.Narro@gmail.com>
2016-09-29 00:50:31 -0400

    style and formatting changes.

--- public/index.php
+++ public/index.php
@@ -17,7 +17,8 @@ ini_set('display_errors', 1); // Display errors on page (instead of a log file)
 </head>
 
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method='GET'>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method='POST'>
+
         <table>
             <tr>
                 <th>Password Generator</th>
@@ -27,43 +28,46 @@ ini_set('display_errors', 1); // Display errors on page (instead of a log file)
             </tr>
             <tr>
                 <td><label><span>Number of words in password:</span></label></td>
-                <td><input type='number' name='wordQty' placeholder="2-9" min="2" max="9"></td>
+                <td><input type='number' name='wordQty' value="<?php echo $wordQty;?>" placeholder="2-9" min="2" max="9"></td>
             </tr>
             <!-- <tr>
-                <td><label><span>Maximum character quantity:</span></label></td>
-                <td><input type='number' name='characterQty' placeholder="8-99"min="8" max="99"></td><br><br>
-            </tr> -->
+            <td><label><span>Maximum character quantity:</span></label></td>
+            <td><input type='number' name='characterQty' placeholder="8-99"min="8" max="99"></td><br><br>
+        </tr> -->
+        <tr>
+            <td><label><span>Include Special Character:</span></label>
+                <td><input type="radio" name="inclChar" value="no" <?php if (isset($inclChar) && $inclChar=="no") echo "checked";?>>no</td>
+                <td><input type="radio" name="inclChar" value="yes" <?php if (isset($inclChar) && $inclChar=="yes") echo "checked";?>>yes</td><br><br>
+            </tr>
             <tr>
-                <td><label><span>Include Special Character:</span></label>
-                    <td><input type="radio" name="inclChar" value="no" checked>no</td>
-                    <td><input type="radio" name="inclChar" value="yes">yes</td><br><br>
-                </tr>
-                <tr>
-                    <td><label><span>Include Integer (0-9)</span></label></td>
-                    <td><input type="radio" name="inclNum" value="no" checked>no</td>
-                    <td><input type="radio" name="inclNum" value="yes">yes</td><br><br>
-                </tr>
-                <tr>
-                    <td><label><span>Delimiting Method:</span></label></td>
-                    <td><input type="radio" name="delimit" value="hyphen" checked>hyphen</td>
-                    <td><input type="radio" name="delimit" value="space">space </td>
-                    <td><input type="radio" name="delimit" value="camelCase">camelCase</td><br><br>
-                </tr>
+                <td><label><span>Include Integer (0-9)</span></label></td>
+                <td><input type="radio" name="inclNum" value="no" <?php if (isset($inclNum) && $inclNum=="no") echo "checked";?>>no</td>
+                <td><input type="radio" name="inclNum" value="yes"<?php if (isset($inclNum) && $inclNum =="yes") echo "checked";?>>yes</td><br><br>
+            </tr>
+            <tr>
+                <td><label><span>Delimiting Method:</span></label></td>
+                <td><input type="radio" name="delimit" value="hyphen" <?php if (isset($delimit) && $delimit=="-") echo "checked";?>>hyphen</td>
+                <td><input type="radio" name="delimit" value="space" <?php if (isset($delimit) && $delimit==" ") echo "checked";?>>space </td>
+                <td><input type="radio" name="delimit" value="camelCase" <?php if (isset($delimit) && $delimit=="") echo "checked";?>>camelCase(no spaces)</td><br><br>
+            </tr>
 
-                <tr>
-                    <td><input type='submit' value='Generate Special Password'></td>
-                </tr>
-                <br><br>
-            </table>
-        </form>
-
-        <table id='results'>
-<tr>
-    <td><?php echo $newPassword ?></td>
-</tr>
+            <tr>
+                <td><input type='submit' value='Generate Special Password'></td>
+            </tr>
+            <br><br>
         </table>
-        <br>
+    </form>
 
-    </body>
+    <table id='results'>
+        <tr>
+            <th>New Password</th>
+        </tr>
+        <tr>
+            <td><?php echo $newPassword ?></td>
+        </tr>
+    </table>
+    <br>
 
-    </html>
+</body>
+
+</html>
--- public/logic.php
+++ public/logic.php
@@ -14,39 +14,49 @@ $newArray[] = shuffle($wordArray);
 $newerArray[] = shuffle($newArray);
 $wordArray[] = $newerArray;
 
-
-$numWords = $_GET["wordQty"];
+//variables
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $wordQty = $_POST["wordQty"];
+    $delimit = $_POST["delimit"];
+    $inclNum = $_POST["inclNum"];
+    $inclChar = $_POST["inclChar"];
+} else {
+    $wordQty = 2;//default
+    $delimit = "-";//default
+    $inclNum = "no";//default
+    $inclChar = "no";//default
+}
 //Random positions for numbers and special characters
-$numRandomIndex = rand(0,($numWords-1));
+$numRandomIndex = rand(0,($wordQty-1));
 $numRandomPos = rand(0,1);
-$charRandomIndex = rand(0,($numWords-1));
+$charRandomIndex = rand(0,($wordQty-1));
 $charRandomPos = rand(0,1);
 
 
 //user selected delimiter
-if ($_GET['delimit'] == 'hyphen') {
-    $delimiter = "-";
-}elseif ($_GET['delimit'] == 'space') {
-    $delimiter = " ";
+if ($_POST['delimit'] == 'hyphen') {
+    $delimit = "-";
+}elseif ($_POST['delimit'] == 'space') {
+    $delimit = " ";
 }else {
-    $delimiter = "";
-    for ($x = 1; $x < $numWords; $x++) {
+    $delimit = "";
+    for ($x = 1; $x < $wordQty; $x++) {
         $wordArray[$x] = ucfirst($wordArray[$x]);
     }
 }
 
 //adds a randomly selected special character to a random word.
 //inserts it into random position at the front or back of complete words
-if ($_GET['inclChar'] == 'yes') {
+if ($_POST['inclChar'] == 'yes') {
     $randomChar = $charArray[array_rand($charArray,1)];
     switch ($charRandomPos) {
         case 0:
-            $wordArray[$charRandomIndex] = substr_replace($wordArray[$charRandomIndex], $randomChar, 0, 0);
-            break;
+        $wordArray[$charRandomIndex] = substr_replace($wordArray[$charRandomIndex], $randomChar, 0, 0);
+        break;
         case 1:
-            $temp = $wordArray[$charRandomIndex].$randomChar;
-            $wordArray[$charRandomIndex] = $temp;
-            break;
+        $temp = $wordArray[$charRandomIndex].$randomChar;
+        $wordArray[$charRandomIndex] = $temp;
+        break;
         default:
         //do nothing
     }
@@ -54,27 +64,31 @@ if ($_GET['inclChar'] == 'yes') {
 
 //adds a randomly selected number to a random word.
 //inserts it into random position at the front or back of complete words
-if ($_GET['inclNum'] == 'yes') {
+if ($_POST['inclNum'] == 'yes') {
     $randomNum = rand(0,9);
     switch ($numRandomPos) {
         case 0:
-            $wordArray[$numRandomIndex] = substr_replace($wordArray[$numRandomIndex], $randomNum, 0, 0);
-            break;
+        $wordArray[$numRandomIndex] = substr_replace($wordArray[$numRandomIndex], $randomNum, 0, 0);
+        break;
         case 1:
-            $temp = $wordArray[$numRandomIndex].$randomNum;
-            $wordArray[$numRandomIndex] = $temp;
-            break;
+        $temp = $wordArray[$numRandomIndex].$randomNum;
+        $wordArray[$numRandomIndex] = $temp;
+        break;
         default:
         //do nothing
     }
 }
 
 //generates a concatenated string of random words with user defined delimiter
-if ($numWords > 0){
-$newPassword = $wordArray[0];
-}
-for ($x = 1; $x < $numWords; $x++) {
-    $addWords = $delimiter . $wordArray[$x];
-    $newPassword.=$addWords;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($wordQty > 0){
+        $newPassword = $wordArray[0];
+    }
+    for ($x = 1; $x < $wordQty; $x++) {
+        $addWords = $delimit . $wordArray[$x];
+        $newPassword.=$addWords;
+    }
+}else {
+    $newPassword ="";
 }
 //echo 'concatenated password is--->'.$newPassword.'<br';
