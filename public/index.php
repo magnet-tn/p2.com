a759e410f36ecf30c773a47583c766b56f80db69
Thomas Narro <Thomas.Narro@gmail.com>
2016-09-30 20:05:57 -0400

    fixed minor validator issue.

--- public/index.php
+++ public/index.php
@@ -30,7 +30,7 @@
 
             <tr>
                 <td><label>Number of words in password: <span>(2-20)</span></label></td>
-                <td><input type='number' name='wordQty' value="<?php echo $wordQty;?>" <?php if (!isset($newPassword)) echo "class='highlight' " ?>placeholder="2-20" min="2" max="20"></td>
+                <td><input type='number' name='wordQty' required value="<?php echo $wordQty;?>" <?php if (!isset($newPassword)) echo "class='highlight' " ?>placeholder="2-20" min="2" max="20"></td>
             </tr>
             <!-- <tr>
             <td><label>Maximum character quantity:</label></td>
@@ -39,21 +39,21 @@
 
         <tr>
             <td><label>Include Special Character:</label>
-                <td><input type="radio" name="inclChar" value="no" <?php if (isset($inclChar) && $inclChar=="no") echo "checked";?>>no</td>
-                <td><input type="radio" name="inclChar" value="yes" <?php if (isset($inclChar) && $inclChar=="yes") echo "checked";?>>yes</td><br><br>
+                <td><input type="radio" name="inclChar" value="no" <?php if (isset($inclChar) && $inclChar=="no") echo "checked";?> >no</td>
+                <td><input type="radio" name="inclChar" value="yes" <?php if (isset($inclChar) && $inclChar=="yes") echo "checked";?> >yes</td><br><br>
             </tr>
 
             <tr>
                 <td><label>Include Integer (0-9)</label></td>
-                <td><input type="radio" name="inclNum" value="no" <?php if (isset($inclNum) && $inclNum=="no") echo "checked";?>>no</td>
-                <td><input type="radio" name="inclNum" value="yes"<?php if (isset($inclNum) && $inclNum =="yes") echo "checked";?>>yes</td><br><br>
+                <td><input type="radio" name="inclNum" value="no" <?php if (isset($inclNum) && $inclNum=="no") echo "checked";?> >no</td>
+                <td><input type="radio" name="inclNum" value="yes"<?php if (isset($inclNum) && $inclNum =="yes") echo "checked";?> >yes</td><br><br>
             </tr>
 
             <tr>
                 <td><label>Delimiting Method:</label></td>
-                <td><input type="radio" name="delimit" value="hyphen" <?php if (isset($delimit) && $delimit=="-") echo "checked";?>>hyphen</td>
-                <td><input type="radio" name="delimit" value="space" <?php if (isset($delimit) && $delimit==" ") echo "checked";?>>space </td>
-                <td><input type="radio" name="delimit" value="camelCase" <?php if (isset($delimit) && $delimit=="") echo "checked";?>>camelCase(no spaces)</td><br><br>
+                <td><input type="radio" name="delimit" value="hyphen" <?php if (isset($delimit) && $delimit=="-") echo "checked";?> >hyphen</td>
+                <td><input type="radio" name="delimit" value="space" <?php if (isset($delimit) && $delimit==" ") echo "checked";?> >space </td>
+                <td><input type="radio" name="delimit" value="camelCase" <?php if (isset($delimit) && $delimit=="") echo "checked";?> >camelCase(no spaces)</td><br><br>
             </tr>
 
             <tr>
@@ -68,7 +68,7 @@
             <th>New Password<?php if (!isset($newPassword)) echo '<em>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(please indicate number of words for your password)</em>' ?></th>
         </tr>
 
-        <tr<?php if(isset($newPassword)) echo " class='output'" ?>>
+        <tr<?php if(isset($newPassword)) echo " class='output'" ?> >
             <td><?php if (isset($newPassword)) echo $newPassword ?></td>
         </tr>
     </table>
